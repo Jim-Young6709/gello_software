17de9dc91e43e4df1ad595f8c2b773e1d1789a37
Jim Young <jiahuiyang6709@outlook.com>
2024-04-08 18:15:13 -0400

    finalized gello config for tabletop setup

--- gello/agents/gello_agent.py
+++ gello/agents/gello_agent.py
@@ -58,6 +58,20 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
         joint_signs=(1, -1, -1, 1, -1, 1),
         gripper_config=None,
     ),
+    # Right xArm850
+    "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT8ISV6J-if00-port0": DynamixelRobotConfig(
+        joint_ids=(1, 2, 3, 4, 5, 6),
+        joint_offsets=(
+            2 * np.pi / 2,
+            2 * np.pi / 2,
+            2 * np.pi / 2,
+            2 * np.pi / 2,
+            2 * np.pi / 2,
+            4 * np.pi / 2,
+        ),
+        joint_signs=(1, -1, -1, 1, -1, 1),
+        gripper_config=None,
+    ),
     # laptop port
     "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT8ISV6J-if00-port0": DynamixelRobotConfig(
         joint_ids=(1, 2, 3, 4, 5, 6),
