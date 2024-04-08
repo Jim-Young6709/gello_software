e7382d52a13729e473f73cac337a8883ed1fa1c9
Jim Young <jiahuiyang6709@outlook.com>
2024-04-08 15:02:25 -0400

    modify gello port configs

--- gello/agents/gello_agent.py
+++ gello/agents/gello_agent.py
@@ -44,7 +44,7 @@ class DynamixelRobotConfig:
 
 
 PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
-    # xArm6
+    # Left xArm850
     "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT8ISVLF-if00-port0": DynamixelRobotConfig(
         joint_ids=(1, 2, 3, 4, 5, 6),
         joint_offsets=(
@@ -55,7 +55,20 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
             2 * np.pi / 2,
             2 * np.pi / 2,
         ),
-        joint_signs=(1, 1, 1, 1, 1, 1),
+        joint_signs=(1, -1, -1, 1, -1, 1),
+        gripper_config=None,
+    ),
+    "usb-FTDI_USB__-__Serial_Converter_FT8ISVLF-if00-port0": DynamixelRobotConfig(
+        joint_ids=(1, 2, 3, 4, 5, 6),
+        joint_offsets=(
+            2 * np.pi / 2,
+            2 * np.pi / 2,
+            2 * np.pi / 2,
+            2 * np.pi / 2,
+            2 * np.pi / 2,
+            0 * np.pi / 2,
+        ),
+        joint_signs=(1, -1, -1, 1, -1, 1),
         gripper_config=None,
     ),
     # ur
